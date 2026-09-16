64f67f3efe34049b14a4fa4dacc67c46915792c5
Andrés Muñoz <andres2078@gmail.com>
2026-09-16 17:26:07 -0500

    feat: add initial task controller

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { body, param } from 'express-validator';
 import { ProjectController } from '../controllers/ProjectController';
 import { handleInputErrors } from '../middleware/validation';
+import { TaskController } from '../controllers/TaskController';
 
 const router = Router();
 
@@ -37,6 +38,11 @@ router.delete(
   param('id').isMongoId().withMessage('ID no válido'),
   handleInputErrors,
   ProjectController.deleteProject,
+);
+
+/** Routes for tasks */
+router.post('/:projectId/tasks',
+  TaskController.createProject
 );
 
 export default router;
