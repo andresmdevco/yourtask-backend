c611e8f23aa26632b5140ba49416baa8372898a8
Andrés Muñoz <andres2078@gmail.com>
2026-09-15 21:02:38 -0500

    feat: add endpoint to get project by ID

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -21,4 +21,20 @@ export class ProjectController {
       console.log(error);
     }
   };
+
+  static getProjectById = async (req: Request, res: Response) => {
+    const {id} = req.params
+    console.log(id)
+    try {
+      const project = await Project.findById(id);
+
+      if(!project) {
+        const error = new Error('Proyecto no encontrado')
+        return res.status(404).json({error: error.message})
+      }
+      res.json(project);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
