caa512039cf4fa3be796714c0df200eba87423b2
Andrés Muñoz <andres2078@gmail.com>
2026-09-17 14:38:41 -0500

    feat: populate project information in tasks

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -16,7 +16,7 @@ export class TaskController {
 
   static getProjectTask = async (req: Request, res: Response) => {
     try {
-      const tasks = await Task.find({ project: req.project._id });
+      const tasks = await Task.find({ project: req.project._id }).populate('project');
       res.json(tasks);
     } catch (error) {
       res.status(500).json({ error: 'Hubo un error' });
