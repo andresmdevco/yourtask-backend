7ad7d6267c823d667cd214ecf63a2fe9ec8bcc8f
Andrés Muñoz <andres2078@gmail.com>
2026-09-17 17:06:12 -0500

    feat: add endpoint to update tasks

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -65,4 +65,10 @@ router.put('/:projectId/tasks/:taskId',
   TaskController.updateTask,
 );
 
+router.delete('/:projectId/tasks/:taskId',
+  param('taskId').isMongoId().withMessage('ID no válido'),
+  handleInputErrors,
+  TaskController.deleteTask,
+);
+
 export default router;
