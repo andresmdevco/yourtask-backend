fb4f66cbc561f9da686b2a4122e40773fe6ee2fc
Andrés Muñoz <andres2078@gmail.com>
2026-09-17 20:34:11 -0500

    refactor: validate task project relationship with middleware

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -25,10 +25,6 @@ export class TaskController {
 
   static getTaskById = async (req: Request, res: Response) => {
     try {
-      if (req.task.project.toString() !== req.project._id.toString()) {
-        const error = new Error('Acción no válida');
-        return res.status(400).json({ error: error.message });
-      }
       res.json(req.task);
     } catch (error) {
       res.status(500).json({ error: 'Hubo un error' });
@@ -37,10 +33,6 @@ export class TaskController {
 
   static updateTask = async (req: Request, res: Response) => {
     try {
-      if (req.task.project.toString() !== req.project._id.toString()) {
-        const error = new Error('Acción no válida');
-        return res.status(400).json({ error: error.message });
-      }
       req.task.name = req.body.name;
       req.task.description = req.body.description;
       await req.task.save();
